3180724ac82b6029f87920c836472bb2c63c968d
Jeffrey Smith <jeffrey.smith7@gmail.com>
2022-03-25 22:42:49 -0400

    Moved custom-variables move to another file since it's really annoying

---
+++ custom.el
@@ -0,0 +1,23 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("c433c87bd4b64b8ba9890e8ed64597ea0f8eb0396f4c9a9e01bd20a04d15d358" "0fffa9669425ff140ff2ae8568c7719705ef33b7a927a0ba7c5e2ffcfac09b75" "2809bcb77ad21312897b541134981282dc455ccd7c14d74cc333b6e549b824f3" "830877f4aab227556548dc0a28bf395d0abe0e3a0ab95455731c9ea5ab5fe4e1" "b89ae2d35d2e18e4286c8be8aaecb41022c1a306070f64a66fd114310ade88aa" default))
+ '(org-agenda-custom-commands
+   '(("A" "Agenda and all NEXT MUSIC"
+      ((agenda ""
+               ((org-agenda-span 'day)))
+       (tags-todo "MUSIC+TODO=\"NEXT\"" nil))
+      nil)))
+ '(org-agenda-files '("~/org/test.org"))
+ '(package-selected-packages
+   '(company-clang auto-package-update highlight-defined dashboard helpful geiser-guile cider use-package kdeconnect racket-mode nim-mode vterm pdf-tools lua-mode ace-window cmake-mode counsel swiper evil-collection ivy company-c-headers solarized-theme magit evil))
+ '(warning-suppress-types '((comp))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
--- init.el
+++ init.el
@@ -45,6 +45,8 @@
   :ensure t)
 (use-package nim-mode
   :ensure t)
+(use-package lua-mode
+  :ensure t)
 (use-package solarized-theme
   :ensure t
   :config
@@ -53,9 +55,13 @@
   :ensure t
   :config
   (add-hook 'after-init-hook 'global-company-mode)
-  (add-to-list 'company-backends 'company-c-headers))
+  (add-to-list 'company-backends 'company-clang)
+  (global-company-mode t))
+
 (use-package company-c-headers
-  :ensure t)
+  :ensure t
+  :config
+  (add-to-list 'company-backends 'company-c-headers))
 (use-package ivy
   :ensure t
   :config
@@ -137,38 +143,9 @@
 	      c-basic-offset 4
 	      indent-tabs-mode nil)
 
-(autoload 'lua-mode "lua-mode" "Lua editing mode." t)
-(add-to-list 'auto-mode-alist '("\\.lua$" . lua-mode))
-(add-to-list 'interpreter-mode-alist '("lua" . lua-mode))
-
-
-
 ;;Backups
 (setq backup-directory-alist '(("." . "~/.emacs.d/saves")))
 (setq backup-by-copying t)
-(setq inhibit-startup-screen nil)
+(setq custom-file (concat user-emacs-directory "/custom.el"))
 
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   '("c433c87bd4b64b8ba9890e8ed64597ea0f8eb0396f4c9a9e01bd20a04d15d358" "0fffa9669425ff140ff2ae8568c7719705ef33b7a927a0ba7c5e2ffcfac09b75" "2809bcb77ad21312897b541134981282dc455ccd7c14d74cc333b6e549b824f3" "830877f4aab227556548dc0a28bf395d0abe0e3a0ab95455731c9ea5ab5fe4e1" "b89ae2d35d2e18e4286c8be8aaecb41022c1a306070f64a66fd114310ade88aa" default))
- '(org-agenda-custom-commands
-   '(("A" "Agenda and all NEXT MUSIC"
-      ((agenda ""
-               ((org-agenda-span 'day)))
-       (tags-todo "MUSIC+TODO=\"NEXT\"" nil))
-      nil)))
- '(org-agenda-files '("~/org/test.org"))
- '(package-selected-packages
-   '(auto-package-update highlight-defined dashboard helpful geiser-guile cider use-package kdeconnect racket-mode nim-mode vterm company-ebdb pdf-tools lua-mode ace-window cmake-mode counsel swiper evil-collection ivy company-c-headers solarized-theme magit evil))
- '(warning-suppress-types '((comp))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
