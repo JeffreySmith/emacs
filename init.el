1702890ad89bd278dc9834f179ae948d8e1271f2
Jeffrey Smith <jeffrey.smith7@gmail.com>
2022-03-25 21:46:01 -0400

    Added package autoupdating, as well as deleted some that I won't ever use

--- init.el
+++ init.el
@@ -5,6 +5,15 @@
 (setq package-native-compile t)
 (add-to-list 'exec-path "~/.bin/")
 
+
+;;Auto-update packages
+(use-package auto-package-update
+  :ensure t
+  :config
+  (setq auto-package-update-delete-old-versions t)
+  (setq auto-package-update-hide-results t)
+  (auto-package-update-maybe))
+
 ;; Evil settings
 (use-package evil
   :ensure t
@@ -19,7 +28,9 @@
   (evil-set-initial-state 'dired-mode 'insert)  
   (evil-set-initial-state 'helpful-mode 'insert)
   (evil-set-initial-state 'git-commit-mode 'insert))
-
+(use-package evil-collection
+  :ensure t
+)
 (use-package vterm
   :ensure t)
 (use-package cider
@@ -27,6 +38,8 @@
 (use-package geiser-guile
   :ensure t)
 (use-package racket-mode
+  :ensure t)
+(use-package nim-mode
   :ensure t)
 (use-package solarized-theme
   :ensure t
@@ -37,6 +50,8 @@
   :config
   (add-hook 'after-init-hook 'global-company-mode)
   (add-to-list 'company-backends 'company-c-headers))
+(use-package company-c-headers
+  :ensure t)
 (use-package ivy
   :ensure t
   :config
@@ -71,6 +86,11 @@
   (setq kdeconnect-active-device "29d7fe6628f4b696"))
 (use-package speed-type
   :ensure t)
+(use-package highlight-defined
+  :ensure t
+  :config
+  (add-hook 'emacs-lisp-mode-hook 'highlight-defined-mode))
+
 (use-package cmake-mode
   :ensure t
   :mode "CMakeLists.txt")
@@ -130,7 +150,21 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("c433c87bd4b64b8ba9890e8ed64597ea0f8eb0396f4c9a9e01bd20a04d15d358" "0fffa9669425ff140ff2ae8568c7719705ef33b7a927a0ba7c5e2ffcfac09b75" "2809bcb77ad21312897b541134981282dc455ccd7c14d74cc333b6e549b824f3" "830877f4aab227556548dc0a28bf395d0abe0e3a0ab95455731c9ea5ab5fe4e1" "b89ae2d35d2e18e4286c8be8aaecb41022c1a306070f64a66fd114310ade88aa" default))
+ '(org-agenda-custom-commands
+   '(("A" "Agenda and all NEXT MUSIC"
+      ((agenda ""
+               ((org-agenda-span 'day)))
+       (tags-todo "MUSIC+TODO=\"NEXT\"" nil))
+      nil)))
  '(org-agenda-files '("~/org/test.org"))
  '(package-selected-packages
-   '(dashboard helpful geiser-guile cider use-package kdeconnect racket-mode nim-mode vterm company-ebdb haskell-mode pdf-tools lua-mode ace-window cmake-project cmake-mode counsel swiper evil-collection ivy company-c-headers solarized-theme magit gruvbox-theme evil))
+   '(auto-package-update highlight-defined dashboard helpful geiser-guile cider use-package kdeconnect racket-mode nim-mode vterm company-ebdb pdf-tools lua-mode ace-window cmake-mode counsel swiper evil-collection ivy company-c-headers solarized-theme magit evil))
  '(warning-suppress-types '((comp))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
