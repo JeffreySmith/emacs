d8666318cbc0930df6916e19e62b880ef787bd80
Jeffrey Smith <jeffrey.smith7@gmail.com>
2022-03-25 21:23:40 -0400

    Moved the ugly stuff to the bottom of init.el

--- init.el
+++ init.el
@@ -1,29 +1,7 @@
 (require 'package)
 (package-initialize)
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   '("c433c87bd4b64b8ba9890e8ed64597ea0f8eb0396f4c9a9e01bd20a04d15d358" "0fffa9669425ff140ff2ae8568c7719705ef33b7a927a0ba7c5e2ffcfac09b75" "2809bcb77ad21312897b541134981282dc455ccd7c14d74cc333b6e549b824f3" "830877f4aab227556548dc0a28bf395d0abe0e3a0ab95455731c9ea5ab5fe4e1" "b89ae2d35d2e18e4286c8be8aaecb41022c1a306070f64a66fd114310ade88aa" default))
- '(org-agenda-custom-commands
-   '(("A" "Agenda and all NEXT MUSIC"
-      ((agenda ""
-               ((org-agenda-span 'day)))
-       (tags-todo "MUSIC+TODO=\"NEXT\"" nil))
-      nil)))
- '(org-agenda-files '("~/org/test.org"))
- '(package-selected-packages
-   '(dashboard helpful geiser-guile cider use-package kdeconnect racket-mode nim-mode vterm company-ebdb haskell-mode pdf-tools lua-mode ace-window cmake-project cmake-mode counsel swiper evil-collection ivy company-c-headers solarized-theme magit gruvbox-theme evil))
- '(warning-suppress-types '((comp))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+
 (setq package-native-compile t)
 (add-to-list 'exec-path "~/.bin/")
 
@@ -110,8 +88,6 @@
   :config
   (dashboard-setup-startup-hook)
   (setq initial-buffer-choice (lambda () (get-buffer "*dashboard*")))
-  (add-to-list 'dashboard-items '(agenda) t)
-  (setq dashboard-week-agenda t)
   (setq dashboard-set-footer nil))
 
 (global-display-line-numbers-mode)
@@ -147,3 +123,14 @@
 (setq backup-directory-alist '(("." . "~/.emacs.d/saves")))
 (setq backup-by-copying t)
 (setq inhibit-startup-screen nil)
+
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(org-agenda-files '("~/org/test.org"))
+ '(package-selected-packages
+   '(dashboard helpful geiser-guile cider use-package kdeconnect racket-mode nim-mode vterm company-ebdb haskell-mode pdf-tools lua-mode ace-window cmake-project cmake-mode counsel swiper evil-collection ivy company-c-headers solarized-theme magit gruvbox-theme evil))
+ '(warning-suppress-types '((comp))))
