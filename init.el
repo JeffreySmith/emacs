29629c792c7ac4de91b8b10183dcc86d2281cd74
Jeffrey Smith <jeffrey.smith7@gmail.com>
2022-03-29 17:39:34 -0400

    I've added all sorts of things to this file

--- init.el
+++ init.el
@@ -4,7 +4,18 @@
 
 (setq package-native-compile t)
 (add-to-list 'exec-path "~/.bin/")
+;;(load "~/.emacs.d/custom.el")
 
+(use-package emacs
+   :bind
+   ("C-=" . 'text-scale-increase)
+   ("C--" . 'text-scale-decrease))
+(use-package gcmh
+  :ensure t
+  :config
+  (gcmh-mode 1))
+(use-package mixed-pitch
+  :ensure t)
 
 ;;Auto-update packages
 (use-package auto-package-update
@@ -12,6 +23,7 @@
   :config
   (setq auto-package-update-delete-old-versions t)
   (setq auto-package-update-hide-results t)
+  (setq auto-package-update-interval 2)
   (auto-package-update-maybe))
 
 ;; Evil settings
@@ -23,9 +35,11 @@
   :config ;; Tweak after it loads
   (evil-mode)
   (add-hook 'org-log-buffer-setup-hook 'evil-insert-state)
+  (add-hook 'org-capture-mode-hook 'evil-insert-state)
   (evil-set-initial-state 'vterm-mode 'insert)
   (evil-set-initial-state 'dired-mode 'insert)  
   (evil-set-initial-state 'helpful-mode 'insert)
+  (evil-set-initial-state 'calendar-mode 'emacs)
   (evil-set-initial-state 'git-commit-mode 'insert))
 (use-package evil-collection
   :ensure t
@@ -51,17 +65,21 @@
   :ensure t
   :config
   (load-theme 'solarized-light t))
+
+
 (use-package company
   :ensure t
   :config
   (add-hook 'after-init-hook 'global-company-mode)
   (add-to-list 'company-backends 'company-clang)
   (global-company-mode t))
-
+(use-package company-box
+  :disabled t)
 (use-package company-c-headers
   :ensure t
   :config
   (add-to-list 'company-backends 'company-c-headers))
+
 (use-package ivy
   :ensure t
   :config
@@ -100,7 +118,10 @@
   :ensure t
   :config
   (add-hook 'emacs-lisp-mode-hook 'highlight-defined-mode))
-
+(use-package smartparens
+  :ensure t
+  :config
+  (require 'smartparens-config))
 (use-package cmake-mode
   :ensure t
   :mode "CMakeLists.txt")
@@ -113,16 +134,28 @@
   ("C-h v" . 'helpful-variable)
   ("C-h k" . 'helpful-variable)
   ("C-c C-d" . 'helpful-at-point))
+(use-package which-key
+  :ensure t
+  :config
+  (setq which-key-idle-delay 4)
+  (which-key-mode))
 (use-package dashboard
   :ensure t
   :config
   (dashboard-setup-startup-hook)
   (setq initial-buffer-choice (lambda () (get-buffer "*dashboard*")))
   (setq dashboard-set-footer nil))
-(use-package org-bullets
+(use-package org-superstar
   :ensure t
   :config
-  (add-hook 'org-mode-hook (lambda () (org-bullets-mode 1))))
+  ;(setq org-superstar-headline-bullets-list '("✖" "✚" "◉" "○" "▶")
+  (setq org-superstar-headline-bullets-list '("✚" "◉" "○" "✸" "✿")     
+        ;; org-superstar-special-todo-items t
+        org-ellipsis " ↴ ")
+  (add-hook 'org-mode-hook (lambda () (org-superstar-mode 1))))
+(use-package org-chef
+  :ensure t)
+  
 (use-package olivetti
   :ensure t)
 (use-package writegood-mode
@@ -131,7 +164,10 @@
   ("C-c C-g" . 'writegood-mode))
 (global-display-line-numbers-mode)
 
-
+;;latitude and longitude
+(setq calendar-latitude 43.6)
+(setq calendar-longitude -79.3)
+(setq calendar-location-name "Toronto, ON")
 ;;Dictionary things
 (setq ispell-program-name "aspell")
 (setq ispell-dictionary "en_CA")
@@ -140,27 +176,96 @@
 (toggle-scroll-bar -1)
 
 ;;Org mode stuff
-(global-set-key (kbd "C-c a") 'org-agenda)
-(global-set-key (kbd "C-c l") 'org-store-link)
 
-(setq org-mobile-directory "~/Dropbox/org")
-(setq org-directory "~/org")
-(setq org-agenda-files (directory-files-recursively "~/org/" "\\.org$"))
-(setq org-log-into-drawer t)
-(setq org-mobile-force-id-on-agenda-items nil)
-(setq org-log-reschedule 'time)
-(setq org-log-done 'time)
-(add-hook 'org-mode-hook '(lambda ()
-                          (visual-line-mode)
-                          (toggle-word-wrap)
-                          (org-indent-mode)
-                          (flyspell-mode)))
+(use-package org-appear
+  :ensure t
+  :hook (org-mode . org-appear-mode))
+
+
+(use-package org
+  :hook
+  (org-mode . visual-line-mode)
+   (org-mode . toggle-word-wrap)
+   (org-mode . flyspell-mode)
+   (prettify-symbols-mode)
+  :bind
+  ("C-c a" . 'org-agenda)
+  ("C-c l" . 'org-store-link)
+  ("<f6>" . 'org-capture)
+  :config
+  (setq org-directory "~/org")
+  (setq org-mobile-directory "~/Dropbox/org")
+  (setq org-agenda-files (directory-files-recursively "~/org/" "\\.org$"))  
+  (setq org-log-into-drawer t)
+  (setq org-mobile-force-id-on-agenda-items nil)
+  (setq org-log-reschedule 'time)
+  (setq org-log-done 'time)
+  (setq org-export-with-smart-quotes t)
+  (setq org-pretty-entities t)
+  (setq org-startup-indented t)
+  (setq org-hide-emphasis-markers t)
+  (setq org-catch-invisible-edits  'smart)
+  
+  (setq org-refile-targets
+        '((nil :maxlevel . 4)
+          (org-agenda-files :maxlevel . 4)))
+  (setq org-outline-path-complete-in-steps nil)
+  (setq org-refile-use-outline-path 'file)
+  (setq org-refile-allow-creating-parent-nodes t)
+  (setq org-capture-templates
+        '(("n" "Test template")
+          ("nt" "TODO entry" entry
+           (file+headline "~/org/test.org" "Capture")
+           "* TODO %^{Description} :NEW:\nDEADLINE: %^t\n%?\n:LOGBOOK:\n- Added: %U\n:END:")
+          ("nb" "Basic entry" entry
+           (file+headline "~/org/test.org" "Capture")
+           "* %^{Description} :NEW:\n%?\n:LOGBOOK:\n- Added: %U\n:END:")
+          ("c" "Cookbook capture")
+          ("cb" "Breakfast Recipe" entry
+           (file+headline "~/org/recipes/cookbook.org" "Breakfast")
+           "%(org-chef-get-recipe-from-url)")
+          ("cp" "Pasta Recipe" entry
+           (file+headline "~/org/recipes/cookbook.org" "Pasta")
+           "%(org-chef-get-recipe-from-url)")
+          ("cu" "Uncategorized" entry
+           (file+headline "~/org/recipes/cookbook.org" "Uncategorized")
+           "%(org-chef-get-recipe-from-url)")
+
+          )))
+
+
+;; (add-hook 'org-mode-hook '(lambda ()
+;;                           (visual-line-mode)
+;;                           (toggle-word-wrap)
+;;                           (org-indent-mode)
+;;                           (flyspell-mode)))
 (setq-default c-default-style "linux"
 	      c-basic-offset 4
 	      indent-tabs-mode nil)
+;; Capture templates
 
 ;;Backups
 (setq backup-directory-alist '(("." . "~/.emacs.d/saves")))
 (setq backup-by-copying t)
 ;;Move variables set by emacs automatically into another file so this one looks clean 
-(setq custom-file (concat user-emacs-directory "/custom.el"))
+;;(setq custom-file (concat user-emacs-directory "/custom.el"))
+
+(load (expand-file-name "~/quicklisp/slime-helper.el"))
+(setq inferior-lisp-program "/usr/bin/sbcl")
+
+;;I tried this for something and I don't think it made a difference
+(setq posframe-gtk-resize-child-frames 'resize-mode)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(which-key org-chef doom-theme mixed-pitch gcmh smartparens org-superstar org-appear writegood-mode vterm use-package speed-type solarized-theme racket-mode pdf-tools olivetti nim-mode magit lua-mode kdeconnect ivy-avy highlight-defined helpful geiser-guile evil-collection ebdb dashboard counsel company-c-headers cmake-mode cider autothemer auto-package-update ace-window)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
