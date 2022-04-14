f2cfbd37d9ff8a6317e2ea9161fb455f47801c84
Jeffrey Smith <jeffrey.smith7@gmail.com>
2022-04-14 13:50:11 -0400

    Added emmet-mode to web-mode-hook

--- init.el
+++ init.el
@@ -101,7 +101,7 @@
 (use-package emmet-mode
   :ensure t
   :config
-  
+  (add-hook 'web-mode-hook 'emmet-mode)
   (add-hook 'sgml-mode-hook 'emmet-mode)
   (add-hook 'css-mode-hook  'emmet-mode))
 (use-package ivy
